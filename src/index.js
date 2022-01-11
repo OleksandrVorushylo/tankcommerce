b331f7eeb9add676fe8c1d11f061468734523fa8
Rhaegar259 <aleksandrvorushilo@gmail.com>
2022-01-11 13:58:00 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -5,16 +5,3 @@ import './sass/style.sass';
 import popup from './modules/popup';
 import slider from './modules/slider';
 
-
-// $(function () {
-// 	$('.popup-modal').magnificPopup({
-// 		type: 'inline',
-// 		preloader: false,
-// 		focus: '#username',
-// 		modal: true
-// 	});
-// 	$(document).on('click', '.popup-modal-dismiss', function (e) {
-// 		e.preventDefault();
-// 		$.magnificPopup.close();
-// 	});
-// });
